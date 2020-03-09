698e859b97d630f310835c419841edb6fa2ea755
xsailing <abcfoobar@outlook.com>
2020-03-09 21:23:20 +0800

    deploy

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   host: 'localhost',
   port: 8000,
   dest: './docs',
-  base: '/docs/',
+  base: '/site/',
   head: [
     ['script', { src: 'https://tajs.qq.com/stats?sId=66403914', charset: 'UTF-8' }]
   ],
